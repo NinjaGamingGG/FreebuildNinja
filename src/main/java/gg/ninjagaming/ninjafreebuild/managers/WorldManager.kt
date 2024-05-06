c6282a6564e2956b4d65098f8c18b58864c84a93
Luca Taschner <iMortalTrix@Proton.me>
2024-05-06 22:58:59 +0200

    Add detailed comments to WorldManager class
    Enhanced code readability by adding detailed comments to the methods in the WorldManager class, which provides utility functions for preparing different types of worlds in the NinjaFreebuild plugin. This documentation includes parameter and return value descriptions, as well as explanations of the methods' functionality and usage.

--- src/main/java/gg/ninjagaming/ninjafreebuild/managers/WorldManager.kt
+++ src/main/java/gg/ninjagaming/ninjafreebuild/managers/WorldManager.kt
@@ -5,9 +5,24 @@ import org.bukkit.*
 import java.io.File
 import kotlin.IllegalArgumentException
 
+/**
+ * The WorldManager class provides utility functions for preparing different types of worlds.
+ */
 class WorldManager {
 
+    /**
+     * The Companion object of the WorldManager class.
+     *
+     * This class provides utility functions for preparing different types of worlds.
+     */
     companion object {
+        /**
+         * Prepares the wilderness world with the specified configurations.
+         * It retrieves the wilderness world name from the configuration file and throws an exception if the name is not found.
+         * Then, it calls the prepareWorld function to prepare the world with the retrieved name.
+         *
+         * @throws IllegalArgumentException if the wilderness world name is not found in the config.yml file.
+         */
         fun prepareWildernessWorld() {
             val worldName = NinjaFreebuild.getConfig().getString("world_configuration.wilderness.world_name")
                 ?: throw IllegalArgumentException("Wilderness world name not found in config.yml")
@@ -15,6 +30,21 @@ class WorldManager {
             prepareWorld(worldName, false)
         }
 
+        /**
+         * Prepares the spawn world with specified configurations.
+         * It retrieves the spawn world name from the configuration file and throws an exception if the name is not found.
+         * Then, it calls the prepareWorld function to prepare the world with the retrieved name.
+         * If the prepareWorld function returns null, it means the world could not be created, and the method returns without further operations.
+         *
+         * The spawn world is configured with the following settings:
+         * - GameRule: DO_DAYLIGHT_CYCLE is set to false
+         * - GameRule: DO_WEATHER_CYCLE is set to false
+         * - Difficulty is set to PEACEFUL
+         * - PvP is disabled
+         * - Time is set to 6000
+         * - Weather clear duration is set to 1 (tick)
+         * - GameRule: DO_MOB_SPAWNING is set to false
+         */
         fun prepareSpawnWorld() {
             val worldName = NinjaFreebuild.getConfig().getString("world_configuration.spawn.world_name")
                 ?: throw IllegalArgumentException("Spawn world name not found in config.yml")
@@ -32,15 +62,32 @@ class WorldManager {
             spawnWorld.setGameRule(GameRule.DO_MOB_SPAWNING, false)
         }
 
-        fun prepareFarmWorld(worldName: String){
 
-            prepareWorld(worldName, true)
+        /**
+         * Prepares a farm world by generating structures and initializing necessary components.
+         *
+         * @param worldName The name of the farm world.
+         * @param generateStructures Indicates whether to generate structures in the farm world. Default is true.
+         */
+        fun prepareFarmWorld(worldName: String, generateStructures: Boolean = true){
+
+            prepareWorld(worldName, generateStructures)
         }
 
+        /**
+         * Prepares a world with the given [worldName] and generates structures if [generateStructures] is true.
+         * If the world is already loaded, it is returned. Otherwise, a new world is created with the specified parameters,
+         * added to the list of Bukkit worlds, and then returned.
+         *
+         * @param worldName The name of the world to prepare.
+         * @param generateStructures Whether to generate structures in the world.
+         * @return The prepared world, or null if it could not be created.
+         */
         private fun prepareWorld(worldName: String, generateStructures: Boolean): World?
         {
             val loadWorld = Bukkit.getWorld(worldName)
 
+            //If world is already loaded return it
             if (loadWorld != null)
                 return loadWorld
 
@@ -51,11 +98,24 @@ class WorldManager {
             return newWorld
         }
 
+        /**
+         * Checks if a world exists in the file system.
+         *
+         * @param worldName The name of the world to check.
+         * @return true if the world exists, false otherwise.
+         */
         fun worldExists(worldName: String): Boolean {
 
             return File("./$worldName").exists()
         }
 
+        /**
+         * Unloads a world with the given name.
+         *
+         * @param woldName The name of the world to unload.
+         * @param save Indicates whether to save the world before unloading it.
+         * @param delete Indicates whether to delete the world folder after unloading it.
+         */
         fun unloadWorld(woldName: String, save: Boolean, delete: Boolean = false)
         {
             val worldFolder = Bukkit.getWorld(woldName)?.worldFolder
